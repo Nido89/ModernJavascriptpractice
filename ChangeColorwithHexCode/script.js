429f86ec4e199171547dab0c9b15fb1ac721a5cc
Naveed <naveed.jalal@edu.novia.fi>
2020-03-01 20:58:17 +0000

    hexcode color changer working

--- ChangeColorwithHexCode/script.js
+++ ChangeColorwithHexCode/script.js
@@ -1,26 +1,23 @@
-
-const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const hexBtn = document.querySelector('.hexbtn');
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hexBtn = document.querySelector('.hexBtn');
 const bodyBcg = document.querySelector('body');
 const hex = document.querySelector('.hex');
-
-function onLoadFunct(){
-    // set Listener here, also using suggested test for null
-    hexBtn.addEventListener('click',getHex);
-}
+hexBtn.addEventListener('click', getHex);
 
 
+function getHex() {
+    let hexCol = '#';
 
-function getHex (){
-let hexCol= '#';
-
-for(let i=0; i<6;i++){
-let rendom = Math.floor(Math.random()*hexNumbers.length);
-//console.log(random);
-hexCol += hexNumbers[random];
-console.log(hexCol);
+    for (let i = 0; i < 6; i++) {
+        let random = Math.floor(Math.random() * hexNumbers.length);
+        console.log(random);
+        hexCol += hexNumbers[random];
+        console.log(hexCol);
+        bodyBcg.style.backgroundColor = hexCol;
+        hex.innerHTML = hexCol;
     }
-    bodyBcg.style.backgroundColor = hexCol;
-    hex.innerHTML = hexCol;
-    
+
+
+
+
 }
