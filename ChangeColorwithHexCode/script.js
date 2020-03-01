5f77355061f8ad2d00a37415836a58c497ea3784
Naveed <naveed.jalal@edu.novia.fi>
2020-03-01 06:50:46 +0000

    hexcolor with background color change

--- ChangeColorwithHexCode/script.js
+++ ChangeColorwithHexCode/script.js
@@ -1,18 +1,26 @@
+
 const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const hexBtn = document.querySelector('.hexbtn');
 const bodyBcg = document.querySelector('body');
 const hex = document.querySelector('.hex');
 
-hexBtn.addEventListener('Click',getHex);
+function onLoadFunct(){
+    // set Listener here, also using suggested test for null
+    hexBtn.addEventListener('click',getHex);
+}
 
-function getHex(){
-    let hexCol= '#';
 
-    for (let i=0; i++){
-        let rendom = Math.floor(Math.random()*hexNumbers.length);
-        hexCol += hexNumbers[random];
-        console.log(hexCol);
 
+function getHex (){
+let hexCol= '#';
+
+for(let i=0; i<6;i++){
+let rendom = Math.floor(Math.random()*hexNumbers.length);
+//console.log(random);
+hexCol += hexNumbers[random];
+console.log(hexCol);
     }
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
     
 }
